3b1b06a8069e64b667700ecd1538b2aa6b882ec8
John Griebel <johnkgriebel@gmail.com>
2017-12-05 08:32:59 -0500

    Update gitignore, requirements, add a setup.py
    Signed-off-by: John Griebel <johnkgriebel@gmail.com>

---
+++ setup.py
@@ -0,0 +1,15 @@
+from setuptools import setup
+
+setup(
+    name="tbs",
+    version="0.0.1",
+    pymodules=['tbs'],
+    install_requires=[
+        'Click',
+    ],
+    dependency_links=["git@github.com:3Blades/python-sdk.git"],
+    entry_points="""
+        [console_scripts]
+        tbs=tbs:cli
+    """
+)
