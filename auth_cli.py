4e2dc42649df8a3090d52d669075cef13d6b467b
John Griebel <johnkgriebel@gmail.com>
2017-12-11 09:29:32 -0500

    Change token file to .config in order to allow additional settings.
    Signed-off-by: John Griebel <johnkgriebel@gmail.com>

--- auth_cli.py
+++ auth_cli.py
@@ -9,14 +9,15 @@ def login(ctx, *args, **kwargs):
     username = kwargs.get('username')
     password = kwargs.get('password')
     home_dir = os.getenv("HOME")
-    token_file = Path(home_dir, ".threeblades.token")
-    if token_file.exists():
-        click.echo(".threeblades.token file already exists in $HOME directory. "
+    config_file = Path(home_dir, ".threeblades.config")
+    if config_file.exists():
+        click.echo(".threeblades.config file already exists in $HOME directory. "
                    "Using that to login.")
-        with token_file.open() as tok_file:
-            line = tok_file.read()
-            token_json = json.loads(line)
-            ctx.obj['token'] = token_json['token']
+        with config_file.open() as conf:
+            line = conf.read()
+            conf_json = json.loads(line)
+            ctx.obj['token'] = conf_json['token']
+            ctx.obj['namespace'] = conf_json.get('namespace', None)
     else:
         if not (username and password):
             username = click.prompt("Username", type=str)
@@ -25,10 +26,17 @@ def login(ctx, *args, **kwargs):
         jwt_data = swagger_client.JWTData(username=username,
                                           password=password)
         response = auth_api.auth_jwt_token_auth(jwt_data=jwt_data)
-        ctx.obj['token'] = response.token
-        with open(token_file, "w") as tok_file:
-            tok_file.write(json.dumps({'token': response.token}))
 
-        click.echo(f"Welcome {username}")
+        config = {'token': response.token,
+                  'namespace': username}
+
+        ctx.obj['token'] = response.token
+        ctx.obj['namespace'] = username
+        with open(config_file, "w") as tok_file:
+            tok_file.write(json.dumps(config))
+
+        click.echo(f"Welcome {username}.")
 
     click.echo("Login successful")
+    click.echo(f"Using {ctx.obj.get('namespace')} as a default namespace. "
+               f"Override this with the --namespace flag.")
--- deployments_cli.py
+++ deployments_cli.py
@@ -2,11 +2,18 @@ import click
 import swagger_client
 
 
-def create(ctx, *args, **kwargs):
-    namespace = kwargs['namespace']
+def deploy(ctx, *args, **kwargs):
+    namespace = kwargs.get('namespace') or ctx.obj.get('namespace')
+    if namespace is None:
+        namespace = click.prompt("Namespace", type=str)
+
     swagger_client.configuration.api_key['Authorization'] = ctx.obj['token']
     swagger_client.configuration.api_key_prefix['Authorization'] = 'Bearer'
-    project_name = click.prompt("Project", type=str)
+
+    project_name = kwargs.get("project")
+    if project_name is None:
+        project_name = click.prompt("Project", type=str)
+
     deployment_name = click.prompt("Name", type=str)
     handler = click.prompt("Handler Function", type=str)
     files = click.prompt("Files", type=str)
@@ -14,7 +21,7 @@ def create(ctx, *args, **kwargs):
     framework = click.prompt("Framework", type=str)
 
     config = swagger_client.DeploymentConfig(handler=handler,
-                                             files=files.split(","))
+                                             files=[f.strip() for f in files.split(",")])
 
     deployment_data = {'name': deployment_name,
                        'config': config,
@@ -24,6 +31,11 @@ def create(ctx, *args, **kwargs):
     deploy_data = swagger_client.DeploymentData(**deployment_data)
 
     deployments_api = swagger_client.DeploymentsApi()
-    response = deployments_api.deployments_create(namespace, project_name,
-                                                  deployment_data=deploy_data)
+    initial_response = deployments_api.deployments_create(namespace=namespace,
+                                                          project=project_name,
+                                                          deployment_data=deploy_data)
+
+    response = deployments_api.deployments_deploy(namespace=namespace,
+                                                  project=project_name,
+                                                  deployment=initial_response.name)
     return response
