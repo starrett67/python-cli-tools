f65996c95dc9770def1075e7f83c0590fd7c3252
John Griebel <johnkgriebel@gmail.com>
2017-12-14 13:11:08 -0500

    Finish the deployments update command.
    Signed-off-by: John Griebel <johnkgriebel@gmail.com>

--- command_groups/deployments.py
+++ command_groups/deployments.py
@@ -80,7 +80,7 @@ class DeploymentsCreateCommand(ThreeBladesBaseCommand):
             handler = click.prompt("Handler Function", type=str)
         kwargs['handler'] = handler
 
-        files = kwargs.get('handler')
+        files = kwargs.get('files')
         if files is None:
             files = click.prompt("Files", type=str)
         kwargs['files'] = files
@@ -227,10 +227,51 @@ class DeploymentsUpdateCommand(ThreeBladesBaseCommand):
         return args, kwargs
 
     def _cmd(self, *args, **kwargs):
+        click.echo("Leave fields blank to maintain their current values.")
+
         name = kwargs.get("name")
         if name is None:
-            name = click.prompt("Name", type=str, default=self.deployment.name)
-        deployment_data = tbs_client.DeploymentData(name=name)
+            name = click.prompt("Name",
+                                type=str,
+                                default=self.deployment.name)
+
+        handler = kwargs.get('handler')
+        if handler is None:
+            handler = click.prompt("Handler Function",
+                                   type=str,
+                                   default=self.deployment.config['handler'])
+
+        files = kwargs.get('files')
+        if files is None:
+            files = click.prompt("Files",
+                                 type=str,
+                                 default=self.deployment.config['files'],
+                                 show_default=False)
+            if isinstance(files, str):
+                # The user entered a new list of files. If they left the default,
+                # files is of type list
+                files = [f.strip() for f in files.split(",")]
+
+        runtime = kwargs.get('runtime')
+        if runtime is None:
+            runtime = click.prompt("Runtime",
+                                   type=str,
+                                   default=self.deployment.runtime)
+
+        framework = kwargs.get('framework')
+        if framework is None:
+            framework = click.prompt("Framework",
+                                     type=str,
+                                     default=self.deployment.framework)
+
+        config = tbs_client.DeploymentConfig(handler=handler,
+                                             files=files)
+
+        deployment_data = tbs_client.DeploymentData(name=name,
+                                                    runtime=RUNTIME_ALIASES.get(runtime) or runtime,
+                                                    framework=FRAMEWORK_ALIASES.get(framework) or framework,
+                                                    config=config)
+
         response = self.api_client.deployments_update(namespace=kwargs['namespace'],
                                                       project=kwargs['project'],
                                                       deployment=kwargs['deployment'],
