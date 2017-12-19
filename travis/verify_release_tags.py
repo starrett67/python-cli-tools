0b2999e0bf5ed9187f6908a6f8756746169b30ee
John Griebel <johnkgriebel@gmail.com>
2017-12-19 12:13:02 -0500

    Better tag and releas message, print what would be created on pull requests.
    Signed-off-by: John Griebel <johnkgriebel@gmail.com>

--- travis/verify_release_tags.py
+++ travis/verify_release_tags.py
@@ -32,21 +32,23 @@ def run(proposed_version: str=os.getenv("TBS_CLI_VERSION")):
         raise ValueError(f"Proposed version {proposed_version} already exists as a release in Github")
     elif sorted(names_only + [proposed_version], key=semver_to_int, reverse=True)[0] != proposed_version:
         raise ValueError(f"Proposed version {proposed_version} would not be the highest version.")
-    elif not os.getenv("TRAVIS_PULL_REQUEST"):
+    else:
         tagger = github.InputGitAuthor(name="3Blades",
                                        email="auto-builds@3blades.io",
                                        date=datetime.now())
-        repo.create_git_tag_and_release(tag=proposed_version,
-                                        # TODO: Create a better tag_message
-                                        tag_message="Created from test script",
-                                        release_name=proposed_version,
-                                        # TODO: Create a better release message
-                                        release_message="Created from test script",
-                                        object=os.getenv("TRAVIS_COMMIT"),
-                                        type="commit",
-                                        tagger=tagger,
-                                        draft=True)
-        print("Successfully created tag and release")
+        kwargs = dict(tag=proposed_version,
+                      tag_message=os.getenv("TRAVIS_COMMIT_MESSAGE"),
+                      release_name=proposed_version,
+                      release_message=os.getenv("TRAVIS_COMMIT_MESSAGE"),
+                      object=os.getenv("TRAVIS_COMMIT"),
+                      type="commit",
+                      tagger=tagger,
+                      draft=True)
+        if os.getenv("TRAVIS_PULL_REQUEST"):
+            print(f"Would be creating a tag and release with the following values:\n{kwargs}")
+        else:
+            repo.create_git_tag_and_release(**kwargs)
+            print("Successfully created tag and release")
 
 
 if __name__ == "__main__":
