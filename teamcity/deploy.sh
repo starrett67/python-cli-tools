88b83292448b61b12c0898faf7a6334bb03187f1
Josh Starrett <jstarrett@3blades.io>
2018-02-08 09:29:26 -0500

    chore: ^teamcity_integration - add logging
    Signed-off-by: Josh Starrett <jstarrett@3blades.io>

--- teamcity/deploy.sh
+++ teamcity/deploy.sh
@@ -1,8 +1,12 @@
 python3.6 -m pip install PyGithub
 
+echo "-- Creating git release draft --"
 export COMMIT_MESSAGE=$(git reflog -1 | sed 's/^.*: //')
 python3.6 teamcity/verify_release_tags.py
-    
+  
 python3.6 -m pip install twine;
+
+echo "-- Packaging module --"
 python3.6 setup.py sdist;
+echo "-- Uploading package --"
 twine upload dist/*;
