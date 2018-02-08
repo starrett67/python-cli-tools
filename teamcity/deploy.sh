f5e174c9ed70e85d158a9bc709e88afd6d9d5174
Josh Starrett <jstarrett@3blades.io>
2018-02-08 09:27:24 -0500

    chore: ^teamcity_integration - get build number from teamcity
    Signed-off-by: Josh Starrett <jstarrett@3blades.io>

--- teamcity/deploy.sh
+++ teamcity/deploy.sh
@@ -1,7 +1,6 @@
 python3.6 -m pip install PyGithub
 
 export COMMIT_MESSAGE=$(git reflog -1 | sed 's/^.*: //')
-export TBS_CLI_VERSION=0.1.1
 python3.6 teamcity/verify_release_tags.py
     
 python3.6 -m pip install twine;
