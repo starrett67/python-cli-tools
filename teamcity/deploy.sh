0cd7fdb7c71036f3cfc0fe8b13285c3def74cd01
Josh Starrett <jstarrett@3blades.io>
2018-02-08 10:04:07 -0500

    chore: ^teamcity_integration - cleanup dist before deployment
    Signed-off-by: Josh Starrett <jstarrett@3blades.io>

--- teamcity/deploy.sh
+++ teamcity/deploy.sh
@@ -4,9 +4,10 @@ bash teamcity/pypirc.template.sh
 echo "-- Creating git release draft --"
 export COMMIT_MESSAGE=$(git reflog -1 | sed 's/^.*: //')
 python3.6 teamcity/verify_release_tags.py
-  
-python3.6 -m pip install twine;
 
+echo "-- Setupt deployment --"
+python3.6 -m pip install twine;
+rm -rf dist
 echo "-- Packaging module --"
 python3.6 setup.py sdist;
 echo "-- Uploading package --"
